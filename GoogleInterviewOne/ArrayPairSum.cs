cf59a447b1d8e9d14697d527fca0e28cc8cd28f6
William Breece <kbd1114@gmail.com>
2020-08-11 21:51:19 -0500

    Added sample google interview solutions.

---
+++ GoogleInterviewOne/ArrayPairSum.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoogleInterviewOne
+{
+    public static class ArrayPairSum
+    {
+        /// <summary>
+        /// Takes a list (in order, ascending) of integers and a sum, returns the items in the list (both value and index) that equal the given sum.
+        /// </summary>
+        /// <param name="values">List of integers to parse</param>
+        /// <param name="sum">Value of 2 summed integers from list, assumed to be positive</param>
+        /// <returns>Key Value pair containing index and value of the found integers</returns>
+        public static KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>> GetPair(List<int> values, int sum)
+        {
+            KeyValuePair<int, int?> first = new KeyValuePair<int, int?>(0, values.First());
+            KeyValuePair<int, int?> second = new KeyValuePair<int, int?>(values.Count - 1, values.Last());
+            while (second.Key - first.Key > 1)
+            {
+                if (first.Value + second.Value == sum)
+                {
+                    return new KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>>(first, second);
+                }
+                else if (first.Value + second.Value > sum)
+                {
+                    var newKey = second.Key - 1;
+                    second = new KeyValuePair<int, int?>(newKey, values[newKey]);
+                }
+                else if (first.Value + second.Value < sum)
+                {
+                    var newKey = first.Key + 1;
+                    first = new KeyValuePair<int, int?>(newKey, values[newKey]);
+                }
+            }
+            return NullKeyValuePair();
+        }
+
+        /// <summary>
+        /// Takes a list (with no assumptions about sorting) of integers and a sum, returns the items in the list (both value and index) that equal the given sum.
+        /// </summary>
+        /// <param name="values">List of integers to parse</param>
+        /// <param name="sum">Value of 2 summed integers from list, assumed to be positive</param>
+        /// <returns>Key Value pair containing index and value of the found integers</returns>
+        public static KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>> GetPairUnsorted(List<int> values, int sum)
+        {
+            HashSet<int> comp = new HashSet<int>();
+
+            foreach (var item in values)
+            {
+                if (comp.Contains(item))
+                {
+                    comp.Add(sum - item);
+                    return new KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>>(
+                        new KeyValuePair<int, int?>(values.IndexOf(item), item),
+                        new KeyValuePair<int, int?>(values.IndexOf(comp.First(x => x == (sum - item))), comp.First(x => x == (sum - item)))
+                        );
+                }
+                comp.Add(sum - item);
+            }
+            return NullKeyValuePair();
+        }
+
+        private static KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>> NullKeyValuePair()
+        {
+            return new KeyValuePair<KeyValuePair<int, int?>, KeyValuePair<int, int?>>(
+                new KeyValuePair<int, int?>(-1, null),
+                new KeyValuePair<int, int?>(-1, null)
+                );
+        }
+    }
+}
---
+++ GoogleInterviewOne/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoogleInterviewOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOne = new List<int> { 0, 1, 2, 3, 4 };
+            var listTwo = new List<int> { 5, 6, 7, 8, 9 };
+            var listThree = new List<int> { -3, -2, 1, 5, 6, 7, 11, 14, 18, 21, 22 };
+            var resOne = ArrayPairSum.GetPair(listOne, 3);
+            var resTwo = ArrayPairSum.GetPair(listOne, 8);
+            var resThree = ArrayPairSum.GetPair(listTwo, 13);
+            var resFour = ArrayPairSum.GetPair(listTwo, 3);
+            var resFive = ArrayPairSum.GetPair(listThree, 32);
+            var resSix = ArrayPairSum.GetPair(listThree, 9);
+
+            Console.WriteLine(resOne);
+            Console.WriteLine(resTwo);
+            Console.WriteLine(resThree);
+            Console.WriteLine(resFour);
+            Console.WriteLine(resFive);
+            Console.WriteLine(resSix);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            resOne = ArrayPairSum.GetPairUnsorted(listOne, 3);
+            resTwo = ArrayPairSum.GetPairUnsorted(listOne, 8);
+            resThree = ArrayPairSum.GetPairUnsorted(listTwo, 13);
+            resFour = ArrayPairSum.GetPairUnsorted(listTwo, 3);
+            resFive = ArrayPairSum.GetPairUnsorted(listThree, 32);
+            resSix = ArrayPairSum.GetPairUnsorted(listThree, 9);
+
+            Console.WriteLine(resOne);
+            Console.WriteLine(resTwo);
+            Console.WriteLine(resThree);
+            Console.WriteLine(resFour);
+            Console.WriteLine(resFive);
+            Console.WriteLine(resSix);
+
+            // -3, -2, 1, 5, 6, 7, 11, 14, 18, 21, 22, sum 32
+        }
+    }
+}
